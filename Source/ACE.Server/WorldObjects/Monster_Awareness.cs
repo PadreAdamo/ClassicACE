d7ce37a434c038a7c3d25912a13b7c82a16fc497
Adam (Padre) <padreadamo@gmail.com>
2023-07-23 00:43:47 -0400

    Update Monster_Awareness.cs
    increased deception/taunt multipliers

--- Source/ACE.Server/WorldObjects/Monster_Awareness.cs
+++ Source/ACE.Server/WorldObjects/Monster_Awareness.cs
@@ -202,9 +202,9 @@ namespace ACE.Server.WorldObjects
                     Entity.CreatureSkill skill = target.GetCreatureSkill(Skill.Deception);
 
                     var activationChance = ThreadSafeRandom.Next(0.0f, 1.0f);
-                    if (skill.AdvancementClass == SkillAdvancementClass.Specialized && activationChance > 1.0)
+                    if (skill.AdvancementClass == SkillAdvancementClass.Specialized && activationChance > 3.0)
                         continue;
-                    else if (skill.AdvancementClass == SkillAdvancementClass.Trained && activationChance > 0.5)
+                    else if (skill.AdvancementClass == SkillAdvancementClass.Trained && activationChance > 1.0)
                         continue;
                     else if (activationChance > 0.10)
                         continue;
@@ -218,7 +218,7 @@ namespace ACE.Server.WorldObjects
                     target.UpdateVitalDelta(target.Mana, -manaCost); // We're past the activation stage so no matter if we succeed or not we consume the mana.
 
                     Entity.CreatureSkill defenseSkill = GetCreatureSkill(Skill.AssessCreature);
-                    var avoidChance = 1.0f - SkillCheck.GetSkillChance(skill.Current, defenseSkill.Current);
+                    var avoidChance = 0.5f - SkillCheck.GetSkillChance(skill.Current, defenseSkill.Current);
 
                     if (avoidChance > ThreadSafeRandom.Next(0.0f, 1.0f))
                     {
