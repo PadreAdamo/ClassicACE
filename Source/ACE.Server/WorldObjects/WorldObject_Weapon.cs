8227cc5d2611819c619e8bba448eff3c40524074
Adam (Padre) <padreadamo@gmail.com>
2023-07-30 22:58:24 -0400

    Update WorldObject_Weapon.cs
    more spear cleaving/piercing work

--- Source/ACE.Server/WorldObjects/WorldObject_Weapon.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Weapon.cs
@@ -96,6 +96,27 @@ namespace ACE.Server.WorldObjects
         }
 
         /// <summary>
+        /// Returns TRUE if this weapon pierces
+        /// </summary>
+        public bool IsPiercing { get => GetProperty(PropertyInt.Cleaving) != null;  }
+
+        public bool IsSpearWeaponProperty
+        {
+            get => GetProperty(PropertyBool.IsSpearWeapon) ?? false;
+            set { if (!value) RemoveProperty(PropertyBool.IsSpearWeapon); else SetProperty(PropertyBool.IsSpearWeapon, value); }
+        }
+
+        public bool IsSpearWeapon
+        {
+            get
+            {
+                if (weapon.Spear == Skill.Spear)
+                    return true;
+                return false;
+            }
+        }
+
+        /// <summary>
         /// Returns the primary weapon equipped by a creature
         /// (melee, missile, or wand)
         /// </summary>
