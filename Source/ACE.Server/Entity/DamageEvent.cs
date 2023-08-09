3e854be8e44798abe6b64cd6cd4844f6a7fa33d2
Adam (Padre) <padreadamo@gmail.com>
2023-08-08 22:54:27 -0400

    Update DamageEvent.cs

--- Source/ACE.Server/Entity/DamageEvent.cs
+++ Source/ACE.Server/Entity/DamageEvent.cs
@@ -318,7 +318,7 @@ namespace ACE.Server.Entity
                             CriticalChance += 0.05f + playerAttacker.ScaleWithPowerAccuracyBar(0.05f);
                         
                         /// Bonus to Axe for critical hit chance
-                        if (Weapon.IsAxe)
+                        if (Weapon.Axe)
                             CriticalChance += 0.08f + playerAttacker.ScaleWithPowerAccuracyBar(0.08f);
                         
                         if (isAttackFromSneaking)
@@ -376,7 +376,7 @@ namespace ACE.Server.Entity
                     IsCritical = true;
 
                      /// Axe bonus to criticaldamagemod
-                    if (Weapon.IsAxe)
+                    if (Weapon.Axe)
                         CriticalDamageMod = 1.3f + WorldObject.GetWeaponCritDamageMod(Weapon, attacker, attackSkill, defender, pkBattle);
 
                     // verify: CriticalMultiplier only applied to the additional crit damage,
