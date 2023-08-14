cc9b0f0fc7cc0903fd191a161474c60ba9458803
Adam (Padre) <padreadamo@gmail.com>
2023-08-13 20:44:15 -0400

    Update Creature_Combat.cs
    ChatGPT recommendations for syntax.

--- Source/ACE.Server/WorldObjects/Creature_Combat.cs
+++ Source/ACE.Server/WorldObjects/Creature_Combat.cs
@@ -719,12 +719,11 @@ namespace ACE.Server.WorldObjects
         public virtual void OnAttackReceived(WorldObject attacker, CombatType attackType, bool critical, bool avoided)
         {
             var attackerAsCreature = attacker as Creature;
-            var debuffskill = attackerAsCreature.TryCastAssessDebuff & attackerAsCreature.TryCastMaceDebuff;
-            
+                   
             if (attackerAsCreature != null)
             {
-//                attackerAsCreature.TryCastAssessDebuff (this, attackType);
-                    debuffskill (this, attackType);
+               attackerAsCreature.TryCastAssessDebuff (this, attackType);
+               attackerAsCreature.TryCastMaceDebuff (this, attackType);
                                 
                 if (!Guid.IsPlayer() && attacker == AttackTarget && (attackType == CombatType.Missile || attackType == CombatType.Magic))
                 {
